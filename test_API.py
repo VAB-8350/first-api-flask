ab3511f9e0f0ead6fd43da83ac194ba65dfaf735
vabklb <v.barilin@itecriocuarto.ort.ar>
2021-09-25 23:52:42 -0300

    pytest

--- test_API.py
+++ test_API.py
@@ -1,7 +1,7 @@
 from function import read_all, validate
 
 def test_get():    
-    cursor = {('pepe', 3, 'formosa'), ('maria', 5, 'cordoba')}
+    cursor = [('pepe', 3, 'formosa'), ('maria', 5, 'cordoba')]
     assert read_all(cursor) == [('pepe', 3, 'formosa'), ('maria', 5, 'cordoba')]
 
 def test_validate_true():
